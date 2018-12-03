b725e2d482c7321ea5db6c82d81d330f22d5fd0b
Estudiante PUCESE <estudiante@pucese.edu.ec>
2018-12-03 08:06:53 -0500

    vector sumatorio y promedio

--- AppVectores/AppVectores/Program.cs
+++ AppVectores/AppVectores/Program.cs
@@ -11,7 +11,36 @@ namespace AppVectores
         static void Main(string[] args)
         {
             Console.WriteLine("control de versiones");
+            vectorinicialzar();
             Console.ReadKey();
+        }
+
+        public static void vectorinicialzar()
+        {
+            int[] x = new int[8];
+            x[0] = 10;
+            x[1]= 2;
+            x[2] = 20;
+            x[3] = 8;
+            x[4] = 15;
+            x[5] = 1;
+            x[6] = -9;
+            x[7] = 14;
+            //imprimir vector
+            Console.WriteLine("\nElementos del vector\n========\n");
+            for (int i=0;i<8;i++)
+            {
+                Console.WriteLine("Elemento indice {0}-->{1}", (i + 1),x[i]);
+            }
+            //sumatoria de los elementos del vector
+            int suma=0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                suma=suma+ x[i];
+
+            }
+            Console.WriteLine("La sumatoria es {0}", suma);
+            Console.WriteLine("La promedio es {0}", suma/x.Length);
         }
     }
 }
